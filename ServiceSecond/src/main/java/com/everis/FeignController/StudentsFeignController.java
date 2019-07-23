700b4692eddb848cfe6886bf46661f86d754190a
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 17:26:59 -0500

    .

--- ServiceSecond/src/main/java/com/everis/FeignController/StudentsFeignController.java
+++ ServiceSecond/src/main/java/com/everis/FeignController/StudentsFeignController.java
@@ -2,14 +2,9 @@ package com.everis.FeignController;
 
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.everis.model.Students;
